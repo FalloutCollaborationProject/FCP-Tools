647e547a6283b4ba60c371abdb5a633f8dd9e539
RickGrymes <81042907+RickGrymes@users.noreply.github.com>
2024-09-20 20:13:51 -0500

    Add files via upload
    made by sirmashedpotato

---
+++ 1.5/Source/Rick_ItemBox/CompProperties_UseEffectItemBox.cs
@@ -0,0 +1,23 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace Rick_ItemBox
+{
+    public class CompProperties_UseEffectItemBox : CompProperties_UseEffect
+    {
+        public CompProperties_UseEffectItemBox()
+        {
+            compClass = typeof(CompUseEffect_ItemBox);
+        }
+
+        public List<ItemDrop> items;
+    }
+
+    public class ItemDrop
+    {
+        public ThingDef thingDef;
+        public IntRange countRange = new IntRange(1,1);
+        public float chance = 1f;
+    }
+}
---
+++ 1.5/Source/Rick_ItemBox/CompUseEffect_ItemBox.cs
@@ -0,0 +1,37 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Rick_ItemBox
+{
+    public class CompUseEffect_ItemBox : CompUseEffect
+    {
+        public CompProperties_UseEffectItemBox Props => (CompProperties_UseEffectItemBox)props;
+
+        public override void DoEffect(Pawn usedBy)
+        {
+            base.DoEffect(usedBy);
+            if (!Props.items.NullOrEmpty())
+            {
+                foreach (ItemDrop drop in Props.items)
+                {
+                    if (Rand.Chance(drop.chance))
+                    {
+                        int count = drop.countRange.RandomInRange;
+                        if (count > 0)
+                        {
+                            DoDrop(drop.thingDef, count);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void DoDrop(ThingDef thingDef, int stackCount)
+        {
+            Thing droppedThing = ThingMaker.MakeThing(thingDef);
+            droppedThing.stackCount = stackCount;
+            droppedThing.TryGetComp<CompQuality>()?.SetQuality(QualityUtility.GenerateQualityRandomEqualChance(), ArtGenerationContext.Colony);
+            GenPlace.TryPlaceThing(droppedThing, parent.Position, parent.Map, ThingPlaceMode.Near);
+        }
+    }
+}
