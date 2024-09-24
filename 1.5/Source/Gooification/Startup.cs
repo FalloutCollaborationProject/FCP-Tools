803bfb07b0d7ba454fad45e606805b1b24c465dc
Taranchuk <nfhfyxer@gmail.com>
2024-09-24 10:27:04 +0300

    gooification rework

---
+++ 1.5/Source/Gooification/Startup.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Verse;
+using System.Text;
+using Verse.AI;
+using HarmonyLib;
+
+namespace FalloutCore
+{
+    [StaticConstructorOnStartup]
+    public static class Startup
+    {
+        static Startup()
+        {
+            new Harmony("FalloutCoreGooification").PatchAll();
+        }
+    }
+}
+
