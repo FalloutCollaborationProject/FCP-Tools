80f2649def80697e9f823fc6a46e266d1249c532
CanonOverseer <agent6841@gmail.com>
2024-09-28 01:24:50 +0800

    Update PermitsCardUtilityPatches.cs

--- 1.5/Source/TitleExtensions/TitleExtensions/PermitsCardUtilityPatches.cs
+++ 1.5/Source/TitleExtensions/TitleExtensions/PermitsCardUtilityPatches.cs
@@ -1,6 +1,5 @@
 ﻿using System.Reflection.Emit;
 using HarmonyLib;
-using UnityEngine;
 
 namespace FCP.TitleExtensions;
 
@@ -21,20 +20,15 @@ public static class PermitsCardUtilityPatches
         // - RoyalTitlePermitDef
         var f_minTitle = AccessTools.Field(typeof(RoyalTitlePermitDef), "minTitle");
         var f_prerequisite = AccessTools.Field(typeof(RoyalTitlePermitDef), "prerequisite");
-        
-        // Methods
-        // - MaxTitlePermisPatches (this)
-        var m_appendMaxTitleStatus = AccessTools.Method(typeof(MaxTitlePermitPatches), nameof(AppendMaxTitleStatus));
 
         // Transpiler Procedure:
         // 1 - Find the minTitle check and the branching point after
         // 2 - Find the next branch which uses permitdef prerequisite and selectedPermit
-        // 3 - Create a label to jump back to.
-        // 4 - Go back one to the end of the previous branch to check it.
-        // 5 - Move back and load the required variables
-        // 6 - Run the code and store the result
+        // 3 - Go back one to the end of the previous branch to check it.
+        // 4 - Move back and load the required variables
+        // 5 - Run the code and store the result
 
-        // 1 - 4
+        // 1 - 3
         var matcher = new CodeMatcher(instructions, generator)
             // 1
             .MatchEndForward(
@@ -49,8 +43,6 @@ public static class PermitsCardUtilityPatches
             )
             .ThrowIfInvalid("PermitsCardUtility_LeftRect_Transpiler: FCPTools couldn't find the second branch (seq 2)")
             // 3
-            .CreateLabel(out var prereqCheckLabel)
-            // 4
             .Advance(-1) // temporarily move back
             .ThrowIfNotMatch(
                 "PermitsCardUtility_LeftRect_Transpiler: instruction prior to end of second branch is not as expected (seq 4)",
@@ -60,14 +52,14 @@ public static class PermitsCardUtilityPatches
         // Get the index for storing text
         int textIndex = (matcher.NamedMatch("storeText").operand as LocalBuilder)!.LocalIndex;
         
-        // 5 - 6
+        // 4 - 5
         matcher.Advance(1) // move back up to the insertion point
             .Insert(
-                // 5
+                // 4
                 CodeInstruction.LoadLocal(textIndex), // text Field
                 CodeInstruction.LoadArgument(1), // Pawn
-                // 6
-                CodeInstruction.Call(typeof(MaxTitlePermitPatches), nameof(AppendMaxTitleStatus)),
+                // 5
+                CodeInstruction.Call(typeof(PermitsCardUtilityPatches), nameof(AppendMaxTitleStatus)),
                 CodeInstruction.StoreLocal(textIndex)
             );
 
