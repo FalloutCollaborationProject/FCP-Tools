e972d1eee583b0d055d989dcf9342feaf8f27af5
CanonOverseer <agent6841@gmail.com>
2024-09-29 23:22:54 +0800

    Add TraderKind specification with a PawnGroupMaker subclass

---
+++ 1.5/Source/TitleExtensions/Factions/GroupMakerWithTraderKind.cs
@@ -0,0 +1,8 @@
+﻿// ReSharper disable FieldCanBeMadeReadOnly.Global
+// ReSharper disable ClassNeverInstantiated.Global
+namespace FCP.Factions;
+
+public class GroupMakerWithTraderKind : PawnGroupMaker
+{
+    public List<TraderKindDef> traderKinds = [];
+}
---
+++ 1.5/Source/TitleExtensions/Factions/PawnGroupKindWorker_Trader_Patches.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+
+namespace FCP.Factions;
+
+[HarmonyPatch(typeof(PawnGroupKindWorker_Trader), "GeneratePawns")]
+public static class PawnGroupKindWorker_Trader_Patches
+{
+    public static void Prefix(PawnGroupMakerParms parms, PawnGroupMaker groupMaker)
+    {
+        if (groupMaker is not GroupMakerWithTraderKind groupMakerWithTrader)
+        {
+            return;
+        }
+        if (groupMakerWithTrader.traderKinds.Empty())
+        {
+            Log.Warning($"FCPTools : A GroupMakerWithTraderKind was defined without any traderKindDefs assigned");
+            return;
+        }
+        
+        parms.traderKind = groupMakerWithTrader.traderKinds.RandomElement();
+    }
+}
