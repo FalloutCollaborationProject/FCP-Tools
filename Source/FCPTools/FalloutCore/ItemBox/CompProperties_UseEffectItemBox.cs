e3c6b4bf9a6f5dfd2aaf8b1a0e7cb9c7f948a6a5
CanonOverseer <agent6841@gmail.com>
2024-09-30 10:41:42 +0800

    Create ItemDrop.cs

--- Source/FCPTools/FalloutCore/ItemBox/CompProperties_UseEffectItemBox.cs
+++ Source/FCPTools/FalloutCore/ItemBox/CompProperties_UseEffectItemBox.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using JetBrains.Annotations;
 using RimWorld;
-using Verse;
 
 namespace FCP.Core;
 
@@ -14,12 +13,4 @@ public class CompProperties_UseEffectItemBox : CompProperties_UseEffect
     }
 
     public List<ItemDrop> items;
-}
-
-[UsedImplicitly]
-public class ItemDrop
-{
-    public ThingDef thingDef;
-    public IntRange countRange = new IntRange(1,1);
-    public float chance = 1f;
 }
---
+++ Source/FCPTools/FalloutCore/ItemBox/ItemDrop.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+using Verse;
+
+namespace FCP.Core;
+
+[UsedImplicitly]
+public class ItemDrop
+{
+    public ThingDef thingDef;
+    public IntRange countRange = new IntRange(1,1);
+    public float chance = 1f;
+}
