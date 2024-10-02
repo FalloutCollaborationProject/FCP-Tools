c7a2c8b72b69b522a578755968512cae780d6b2f
CanonOverseer <agent6841@gmail.com>
2024-10-02 09:19:42 +0800

    Merge some patch files

--- Source/FCPTools/FactionTools/Trading/GroupMakerWithTraderKind.cs
+++ Source/FCPTools/FactionTools/Trading/GroupMakerWithTraderKind.cs
@@ -2,11 +2,32 @@
 // ReSharper disable ClassNeverInstantiated.Global
 
 using System.Collections.Generic;
+using HarmonyLib;
 using RimWorld;
+using Verse;
 
 namespace FCP.Factions;
 
 public class GroupMakerWithTraderKind : PawnGroupMaker
 {
     public List<TraderKindDef> traderKinds = [];
+}
+
+[HarmonyPatch(typeof(PawnGroupKindWorker_Trader), "GeneratePawns")]
+public static class PawnGroupKindWorker_Trader_Patches
+{
+    public static void Prefix(PawnGroupMakerParms parms, PawnGroupMaker groupMaker)
+    {
+        if (groupMaker is not GroupMakerWithTraderKind groupMakerWithTrader)
+        {
+            return;
+        }
+        if (groupMakerWithTrader.traderKinds.Empty())
+        {
+            Log.Warning($"FCPTools : A GroupMakerWithTraderKind was defined without any traderKindDefs assigned");
+            return;
+        }
+        
+        parms.traderKind = groupMakerWithTrader.traderKinds.RandomElement();
+    }
 }
