14308d2527a32156b4b7ae0af5e7e4c7deb2e9e1
Steve <84108749+Scurvyez@users.noreply.github.com>
2024-10-14 22:28:53 -0400

    defof fix......... again

--- Source/FCPTools/FalloutCore/Defs/FCPDefOf.cs
+++ Source/FCPTools/FalloutCore/Defs/FCPDefOf.cs
@@ -4,8 +4,8 @@
     public class FCPDefOf
     {
         [MayRequire("Rick.FCP.Legion")]
-        public static FactionDef FCP_Caesars_Legion;
-    
+        public static FactionDef FCP_Faction_Caesars_Legion;
+        
         [MayRequire("Rick.FCP.NCR")]
         public static FactionDef FCP_Faction_NCR;
     
