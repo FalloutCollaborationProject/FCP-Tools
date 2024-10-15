27d8c9501e9b4df73f0c53ae3f2ef4483561d1ae
Canon <canonoverseer@gmail.com>
2024-10-15 10:21:41 +0800

    Update FCPDefOf to use new Legion defName

--- Source/FCPTools/FalloutCore/Defs/FCPDefOf.cs
+++ Source/FCPTools/FalloutCore/Defs/FCPDefOf.cs
@@ -1,10 +1,12 @@
-﻿namespace FCP.Core
+﻿// ReSharper disable UnassignedField.Global
+// ReSharper disable InconsistentNaming
+namespace FCP.Core
 {
     [DefOf]
     public class FCPDefOf
     {
         [MayRequire("Rick.FCP.Legion")]
-        public static FactionDef FCP_Caesars_Legion;
+        public static FactionDef FCP_Faction_Caesars_Legion;
     
         [MayRequire("Rick.FCP.NCR")]
         public static FactionDef FCP_Faction_NCR;
