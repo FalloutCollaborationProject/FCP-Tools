81ce74fd10b4dfded52d1b9f56e4dd6ac65490cf
Canon <canonoverseer@gmail.com>
2024-11-28 01:37:41 +0800

    Add unstable version warning

--- Source/FCPTools/FalloutCore/FCPCoreMod.cs
+++ Source/FCPTools/FalloutCore/FCPCoreMod.cs
@@ -4,6 +4,7 @@ public class FCPCoreMod : Mod
 {
     public FCPCoreMod(ModContentPack content) : base(content)
     {
-        FCPLog.Message($"Welcome home...");
+        FCPLog.Message("Welcome home...");
+        FCPLog.Error("This is an unstable beta version and bugs are very likely, if not guaranteed!\nRemember to report bugs on steam page or our discord: 3HEXN3Qbn4");
     }
 }
