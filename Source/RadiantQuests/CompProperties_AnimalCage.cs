d51f47117b00da0e35d4b99e912342528d7fabf9
Nalzurin <alexnormandi@ukr.net>
2024-12-05 00:38:43 +0200

    Implement animal cages

---
+++ Source/RadiantQuests/CompProperties_AnimalCage.cs
@@ -0,0 +1,21 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace FCP_RadiantQuests
+{
+    public class CompProperties_AnimalCage : CompProperties_Refuelable
+    {
+        public float maxBodySize = 1f;
+        public float minBodySize = 0f;
+        public List<PawnKindDef> animalsThatGetCaught = new List<PawnKindDef>();
+        public CompProperties_AnimalCage()
+        {
+            compClass = typeof(CompAnimalCage);
+        }
+    }
+}
