d1cfd4e972bad838a1f56983327928e3028dc574
nimragiscoming <nine.seven.ninete@gmail.com>
2024-12-12 19:40:52 +0000

    added mech detonation to the project

---
+++ Source/FCPTools/FalloutCore/ThingComps/CompMechDetonator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FCP.Core
+{
+    public class CompMechDetonator : ThingComp
+    {
+        public CompProperties_MechDetonator Props => (CompProperties_MechDetonator)props;
+
+        public override IEnumerable<Gizmo> CompGetGizmosExtra()
+        {
+
+            if (parent.Faction == Faction.OfPlayer)
+            {
+                Command_Action command_Action = new Command_Action();
+                command_Action.icon = Props.GetUiIcon();
+                command_Action.defaultLabel = "FCP_MechDetonate".Translate();
+                command_Action.action = delegate
+                {
+                    //Log.Message("BOOM");
+                    parent.Kill();
+                };
+                yield return command_Action;
+            }
+
+        }
+    }
+}
---
+++ Source/FCPTools/FalloutCore/ThingComps/CompProperties/CompProperties_MechDetonator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace FCP.Core
+{
+    public class CompProperties_MechDetonator : CompProperties
+    {
+
+        public float radius = 3f;
+        public int damage = -1;
+        public string iconPath;
+        Texture2D uiIcon;
+        public Texture2D GetUiIcon() { return uiIcon; }
+        public CompProperties_MechDetonator()
+        {
+            compClass = typeof(CompMechDetonator);
+        }
+
+        public override void ResolveReferences(ThingDef parentDef)
+        {
+            if (!string.IsNullOrEmpty(iconPath))
+            {
+                LongEventHandler.ExecuteWhenFinished(delegate
+                {
+                    uiIcon = ContentFinder<Texture2D>.Get(iconPath);
+                });
+            }
+        }
+    }
+}
