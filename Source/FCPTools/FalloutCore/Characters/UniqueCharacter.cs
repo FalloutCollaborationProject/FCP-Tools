3c54eb0d3cbfdc137f3f0f19319f8cacd08c163a
Canon <canonoverseer@gmail.com>
2024-12-21 23:46:42 +0800

    Move some functionality to the UniqueCharacter  class itself

--- Source/FCPTools/FalloutCore/Characters/UniqueCharacter.cs
+++ Source/FCPTools/FalloutCore/Characters/UniqueCharacter.cs
@@ -6,7 +6,7 @@ public class UniqueCharacter : IExposable
 {
     public CharacterDef def;
     public Pawn pawn;
-
+    
     public UniqueCharacter(CharacterDef def)
     {
         this.def = def;
@@ -21,12 +21,60 @@ public class UniqueCharacter : IExposable
         return !pawn.Discarded;
     }
 
-    
+    /// <summary>
+    /// Check if the pawn is available in the world (not dead and tracked).
+    /// </summary>
     public bool PawnAvailableInWorld()
     {
         return pawn != null && !pawn.Dead && pawn.IsWorldPawn();
     }
 
+    #region Generation
+    
+    /// <summary>
+    /// Ensure the pawn exists, generating it if necessary.
+    /// </summary>
+    public Pawn EnsurePawnExists(PawnGenerationRequest? baseParams = null, 
+        Func<PawnGenerationRequest, PawnGenerationRequest> modifyGenRequest = null)
+    {
+        if (PawnExists()) 
+            return pawn;
+
+        pawn = GeneratePawn(baseParams, modifyGenRequest);
+
+        // Ensure the pawn is managed by the world pawns and kept from gc (also makes sure it's stored somewhere and saved)
+        Find.WorldPawns.PassToWorld(pawn, PawnDiscardDecideMode.KeepForever);
+
+        return pawn;
+    }
+
+    /// <summary>
+    /// Generate a new pawn based on this character's definition.
+    /// </summary>
+    private Pawn GeneratePawn(
+        PawnGenerationRequest? baseParams = null,
+        Func<PawnGenerationRequest, PawnGenerationRequest> modifyGenRequest = null)
+    {
+        FCPLog.Message($"Generating Unique Pawn: {def.defName}");
+        PawnGenerationRequest request = baseParams ?? new PawnGenerationRequest(def.pawnKind);
+
+        // Defaults
+        request.KindDef ??= def.pawnKind;
+        request.Faction ??= Find.FactionManager.FirstFactionOfDef(def.faction); // May be null for initial faction leaders.
+        request.ForceGenerateNewPawn = true;
+
+        // Apply custom definitions or modifications to the generation request.
+        CharacterDefinitionUtils.ApplyRequestDefinitions(ref request, def.definitions);
+        request = modifyGenRequest?.Invoke(request) ?? request;
+
+        // Generate the pawn and apply post gen definitions
+        Pawn generatedPawn = PawnGenerator.GeneratePawn(request);
+        CharacterDefinitionUtils.ApplyPawnDefinitions(generatedPawn, def.definitions);
+
+        return generatedPawn;
+    }
+    
+    #endregion
 
     public void ExposeData()
     {
--- Source/FCPTools/FalloutCore/Components/UniqueCharactersTracker.cs
+++ Source/FCPTools/FalloutCore/Components/UniqueCharactersTracker.cs
@@ -8,44 +8,26 @@ public class UniqueCharactersTracker : WorldComponent
     
     public static UniqueCharactersTracker Instance { get; private set; }
 
-    private List<UniqueCharacter> characters = new List<UniqueCharacter>();
+    private List<UniqueCharacter> characters = [];
 
     public UniqueCharactersTracker(World world) : base(world)
     {
         Instance = this;
     }
-
-    /// <summary>
-    /// Check for a UniqueCharacter entry in the tracker and if the entry has a non destroyed/discarded pawn.
-    /// </summary>
-    public bool CharacterPawnExists(CharacterDef charDef, bool aliveOnly = false)
-    {
-        UniqueCharacter character = characters.Find(chr => chr.def == charDef);
-        return character != null && character.PawnExists() && (!aliveOnly || !character.pawn.Dead);
-    }
-
-    /// <summary>
-    /// Check for a UniqueCharacter entry in the tracker and if the entry has a non destroyed/discarded living world pawn.
-    /// </summary>
-    public bool CharacterWorldPawnAvailable(CharacterDef charDef)
-    {
-        UniqueCharacter character = characters.Find(chr => chr.def == charDef);
-        return character.PawnAvailableInWorld();
-    }
     
     /// <summary>
     /// Try find a matching UniqueCharacter for a given pawn
     /// </summary>
-    public bool IsUniquePawn(Pawn pawn, out UniqueCharacter character)
+    public bool TryGetCharacterFromPawn(Pawn pawn, out UniqueCharacter character)
     {
         character = characters.Find(chr => chr.pawn == pawn);
         return character != null;
     }
 
     /// <summary>
-    /// Get an existing UniqueCharacter for the given CharacterDef, or create a new one if it doesn't exist.
+    /// Retrieve or create a UniqueCharacter for a given CharacterDef.
     /// </summary>
-    private UniqueCharacter GetOrCreateUniqueCharacter(CharacterDef charDef)
+    public UniqueCharacter GetUniqueCharacter(CharacterDef charDef)
     {
         UniqueCharacter character = characters.Find(chr => chr.def == charDef);
 
@@ -58,64 +40,22 @@ public class UniqueCharactersTracker : WorldComponent
 
         return character;
     }
+    
 
     /// <summary>
     /// Get an existing pawn or generate a new one for the given CharacterDef.
     /// </summary>
-    public Pawn GetOrGenPawn(CharacterDef charDef, 
-        PawnGenerationRequest? baseParams = null,
+    public Pawn GetOrGenPawn(CharacterDef charDef, PawnGenerationRequest? baseParams = null, 
         Func<PawnGenerationRequest, PawnGenerationRequest> modifyGenRequest = null)
     {
-        // Create a new, or get an existing unique character from the tracker
-        UniqueCharacter character = GetOrCreateUniqueCharacter(charDef);
-        
-        if (character.PawnExists())
-        {
-            return character.pawn;
-        }
-        
-        character.pawn = GenerateUniquePawn(charDef, baseParams, modifyGenRequest);
-
-        // Makes sure the pawn is saved somewhere and immune from GC.
-        Find.WorldPawns.PassToWorld(character.pawn, PawnDiscardDecideMode.KeepForever);
-
-        return character.pawn;
-    }
-    
-    /// <summary>
-    /// Generate a new pawn for the given CharacterDef.
-    /// </summary>
-    private static Pawn GenerateUniquePawn(CharacterDef charDef, 
-        PawnGenerationRequest? baseParams = null,
-        Func<PawnGenerationRequest, PawnGenerationRequest> modifyGenRequest = null)
-    {
-        
-        FCPLog.Message($"Generating Unique Pawn: {charDef.defName}");
-        PawnGenerationRequest request = baseParams ?? new PawnGenerationRequest(charDef.pawnKind);
-
-        // Defaults
-        request.KindDef ??= charDef.pawnKind;
-        request.Faction ??= Find.FactionManager.FirstFactionOfDef(charDef.faction); // May be null for initial faction leaders.
-        request.ForceGenerateNewPawn = true;
-
-        // Apply any custom definitions or modifications to the generation request
-        CharacterDefinitionUtils.ApplyRequestDefinitions(ref request, charDef.definitions);
-        request = modifyGenRequest?.Invoke(request) ?? request;
-
-        // Actually Generate the pawn
-        Pawn pawn = PawnGenerator.GeneratePawn(request);
-
-        // Post generation definitions to the actual Pawn
-        CharacterDefinitionUtils.ApplyPawnDefinitions(pawn, charDef.definitions);
-
-        return pawn;
+        UniqueCharacter character = GetUniqueCharacter(charDef);
+        return character.EnsurePawnExists(baseParams, modifyGenRequest);
     }
     
     public override void FinalizeInit()
     {
         base.FinalizeInit();
         Instance = this;
-
     }
 
     public override void ExposeData()
