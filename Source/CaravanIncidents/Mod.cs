c976702f04f05107383e80ec8e0bf209fe3d16d2
Nalzurin <alexnormandi@ukr.net>
2025-01-26 04:11:36 +0200

    Implement settings window

--- Source/CaravanIncidents/Mod.cs
+++ Source/CaravanIncidents/Mod.cs
@@ -32,15 +32,44 @@ namespace FCP_CaravanIncidents
     public class CaravanIncidents_Settings : ModSettings
     {
         public static int shuttleCrashWeightA = 1;
+        private string _shuttleCrashWeightA;
         public static int shuttleCrashWeightB = 500;
+        private string _shuttleCrashWeightB;
         public static int shuttleCrashWeightC = 1;
+        private string _shuttleCrashWeightC;
 
         public static int shuttleWeightsTotal => shuttleCrashWeightA + shuttleCrashWeightB + shuttleCrashWeightC;
         public static int[] cumulativeWeightsShuttleCrash => new int[] { shuttleCrashWeightA, shuttleCrashWeightA + shuttleCrashWeightB, shuttleCrashWeightA + shuttleCrashWeightB + shuttleCrashWeightC};
 
         public void DoWindowContents(Rect inRect)
         {
+/*            Rect rect2 = new Rect(inRect);
+            rect2.height = 750f;
+            Rect rect3 = rect2;
+            Widgets.AdjustRectsForScrollView(inRect, ref rect2, ref rect3);
+            Widgets.BeginScrollView(inRect, ref _scrollPosition, rect3, showScrollbars: false);
+*/
+            Listing_Standard listing_Standard = new Listing_Standard();
+            listing_Standard.Begin(inRect);
 
+            listing_Standard.Gap();
+            using (new TextBlock(GameFont.Medium))
+            {
+                listing_Standard.Label("FCP_CaravanIncident_Settings_ShuttleCrash_Label".Translate());
+
+            }
+
+            listing_Standard.Label("FCP_CaravanIncident_Settings_ShuttleCrash_VariantA".Translate());
+            listing_Standard.TextFieldNumeric(ref shuttleCrashWeightA, ref _shuttleCrashWeightA, 0, 250);
+
+            listing_Standard.Label("FCP_CaravanIncident_Settings_ShuttleCrash_VariantB".Translate());
+            listing_Standard.TextFieldNumeric(ref shuttleCrashWeightB, ref _shuttleCrashWeightB, 0, 250);
+
+            listing_Standard.Label("FCP_CaravanIncident_Settings_ShuttleCrash_VariantC".Translate());
+            listing_Standard.TextFieldNumeric(ref shuttleCrashWeightC, ref _shuttleCrashWeightC, 0, 100);
+
+            listing_Standard.End();
+            //Widgets.EndScrollView();
         }
         public override void ExposeData()
         {
