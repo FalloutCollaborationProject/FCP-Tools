4e9fe760b5e9a109949c8845df265e064135e8a4
RickGrymes <81042907+RickGrymes@users.noreply.github.com>
2025-12-22 23:26:23 -0600

    Add RecipeGoodwillLimiter mod initial implementation
    Introduces the RecipeGoodwillLimiter mod, including Harmony patches to restrict recipe production based on faction goodwill, a mod extension for recipe definitions, and project/solution files for building the mod. The main patch suspends bills if the player's goodwill with a required faction is too low or the faction is defeated, as configured per recipe.

---
+++ Source/RecipeGoodwillLimiter/StartPatch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RecipeGoodwillLimiter
+{
+    [StaticConstructorOnStartup]
+    public static class StartPatch
+    {
+        static StartPatch()
+        {
+            Harmony harmony = new Harmony("FarmerJoe.UnpatcherGeneric");
+            harmony.PatchAll();
+        }
+    }
+}
