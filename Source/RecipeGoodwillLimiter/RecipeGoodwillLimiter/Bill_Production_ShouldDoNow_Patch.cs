97d77de2371447924b1d969e4721d64c0c2b67bd
RickGrymes <81042907+RickGrymes@users.noreply.github.com>
2025-12-27 00:19:10 -0600

    Add Pip-Boy apparel and RecipeGoodwillLimiter mod
    Introduced Pip-Boy 3000 and 3000 Mark IV apparel definitions, textures, and added them as rare drops in lunchboxes. Added the RecipeGoodwillLimiter C# mod, which restricts recipe availability based on faction goodwill, including Harmony patches, mod extension, and project files.

---
+++ Source/RecipeGoodwillLimiter/RecipeGoodwillLimiter/Bill_Production_ShouldDoNow_Patch.cs
@@ -0,0 +1,73 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RecipeGoodwillLimiter
+{
+    [HarmonyPatch]
+    public class Bill_Production_ShouldDoNow_Patch
+    {
+        [HarmonyPatch(typeof(Bill_Production), "ShouldDoNow")]
+        [HarmonyPrefix]
+        public static void ShouldDoNow_GoodWillCheck(ref bool __result, Bill_Production __instance)
+        {
+            Faction playerFaction = Faction.OfPlayer;
+            if (playerFaction == null)
+            {
+                __instance.suspended = true;
+                __result = false;
+            }
+            RecipeExtension_GoodwillCheck modExtension = __instance.recipe.GetModExtension<RecipeExtension_GoodwillCheck>();
+            if (modExtension == null)
+            {
+                return;
+            }
+            try
+            {
+                Faction targetFact = Find.FactionManager.FirstFactionOfDef(modExtension.requireFaction);
+                if (targetFact == null)
+                {
+                    if (!__instance.suspended)
+                    {
+                        Messages.Message("Goodwill_FactionDontExist".Translate(targetFact.Name, modExtension.minimumGoodwill), MessageTypeDefOf.NegativeEvent);
+                    }
+                    __instance.suspended = true;
+                    __result = false;
+                }
+                else
+                {
+                    if (targetFact.defeated)
+                    {
+                        if (modExtension.uncraftableIfFactionDefeated)
+                        {
+                            if (!__instance.suspended)
+                            {
+                                Messages.Message("Goodwill_FactionDefeated".Translate(targetFact.Name, modExtension.minimumGoodwill), MessageTypeDefOf.NegativeEvent);
+                            }
+                            __instance.suspended = true;
+                            __result = false;
+                        }
+                    }
+                    else if (targetFact.GoodwillWith(playerFaction) < modExtension.minimumGoodwill)
+                    {
+                        if (!__instance.suspended)
+                        {
+                            Messages.Message("GoodwillUnmet".Translate(targetFact.Name, modExtension.minimumGoodwill), MessageTypeDefOf.NegativeEvent);
+                        }
+                        __instance.suspended = true;
+                        __result = false;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"[RecipeGoodwillLimiter] Error: {ex}");
+            }
+        }
+    }
+}
---
+++ Source/RecipeGoodwillLimiter/RecipeGoodwillLimiter/RecipeExtension_GoodwillCheck.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace RecipeGoodwillLimiter
+{
+    public class RecipeExtension_GoodwillCheck : DefModExtension
+    {
+        public FactionDef requireFaction;
+
+        public int minimumGoodwill = -1;
+
+        public bool uncraftableIfFactionDefeated = true;
+    }
+}
