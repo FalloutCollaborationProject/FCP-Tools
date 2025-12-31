20ab65056dd83712d771b5dd1e61451226da9fe2
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2025-12-30 22:41:14 -0700

    Change MaxArmor Cap
    *Set to 8f

---
+++ Source/FCPTools/FalloutCore/Harmony/ITab_Gear_TryDrawArmor_Patch.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection.Emit;
+using HarmonyLib;
+
+namespace FCP.Core
+{
+    [HarmonyPatch(typeof(ITab_Pawn_Gear), "TryDrawOverallArmor")]
+    public class ITab_Gear_TryDrawArmor_Patch
+    {
+
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> code = instructions.ToList();
+            for (int i = 1; i < code.Count - 1; i++)
+            {
+                if (code[i].opcode == OpCodes.Ldc_R4 && code[i].operand is 2f)
+                {
+                    code[i].operand = FCP_Utility.MaxArmorValue;
+                }
+            }
+            return code;
+        }
+    }
+}
+
---
+++ Source/FCPTools/FalloutCore/Utilities/FCP_Utility.cs
@@ -0,0 +1,12 @@
+﻿namespace FCP.Core
+{
+    public static class FCP_Utility
+    {
+        #region Global Constants
+
+        public const float MaxArmorValue = 8f;
+
+        #endregion
+
+    }
+}
