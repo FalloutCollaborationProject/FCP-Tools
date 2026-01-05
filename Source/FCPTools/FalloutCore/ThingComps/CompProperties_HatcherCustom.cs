743867807a7733175eb2042040b643728066ef24
ViralReaction <136116069+ViralReaction@users.noreply.github.com>
2026-01-04 20:10:32 -0700

    Add CompHatcherCustom

---
+++ Source/FCPTools/FalloutCore/ThingComps/CompProperties_HatcherCustom.cs
@@ -0,0 +1,17 @@
+﻿namespace FCP.Core
+{
+    public class CompProperties_HatcherCustom : CompProperties
+    {
+        public float hatcherDaystoHatch = 1f;
+
+        public PawnKindDef hatcherPawn;
+        public float secondaryOverrideChance = 0f;
+        public PawnKindDef secondaryPawn;
+
+
+        public CompProperties_HatcherCustom()
+        {
+            compClass = typeof(CompHatcherCustom);
+        }
+    }
+}
