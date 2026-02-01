9fe8a3b9ead868a0508d2e03bb631d8561441d55
Canon <canonoverseer@gmail.com>
2026-02-02 06:02:01 +0800

    Merge RecipeGoodwillLimiter into FCPTools

---
+++ Source/FCPTools/FalloutCore/Harmony/Bill_Production_ShouldDoNow_Patch.cs
@@ -0,0 +1,54 @@
+﻿using HarmonyLib;
+
+namespace FCP.Core;
+
+[HarmonyPatch]
+public class Bill_Production_ShouldDoNow_Patch
+{
+    [HarmonyPatch(typeof(Bill_Production), "ShouldDoNow")]
+    [HarmonyPrefix]
+    public static void ShouldDoNow_GoodWillCheck(ref bool __result, Bill_Production __instance)
+    {
+        Faction playerFaction = Faction.OfPlayer;
+        if (playerFaction == null)
+        {
+            __instance.suspended = true;
+            __result = false;
+        }
+        var modExtension = __instance.recipe.GetModExtension<RecipeExtension_GoodwillCheck>();
+        Faction targetFact = Find.FactionManager.FirstFactionOfDef(modExtension.requireFaction);
+        if (targetFact == null)
+        {
+            if (!__instance.suspended)
+            {
+                Messages.Message("GoodwillUnmet".Translate(targetFact.Name, modExtension.minimumGoodwill), MessageTypeDefOf.NegativeEvent);
+            }
+            __instance.suspended = true;
+            __result = false;
+        }
+        else
+        {
+            if (targetFact.defeated)
+            {
+                if (modExtension.uncraftableIfFactionDefeated)
+                {
+                    if (!__instance.suspended)
+                    {
+                        Messages.Message("GoodwillUnmet".Translate(targetFact.Name, modExtension.minimumGoodwill), MessageTypeDefOf.NegativeEvent);
+                    }
+                    __instance.suspended = true;
+                    __result = false;
+                }
+            }
+            else if (targetFact.GoodwillWith(playerFaction) < modExtension.minimumGoodwill)
+            {
+                if (!__instance.suspended)
+                {
+                    Messages.Message("GoodwillUnmet".Translate(targetFact.Name,modExtension.minimumGoodwill), MessageTypeDefOf.NegativeEvent);
+                }
+                __instance.suspended = true;
+                __result = false;
+            }
+        }
+    }
+}
