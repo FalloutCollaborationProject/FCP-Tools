51407ae87716a979bb97bb81a20c8922bbac636c
Canon <canonoverseer@gmail.com>
2026-02-14 09:52:32 +0800

    Rewrite/Fix RoyalTitlePermitWorker_DropResources_Patch

--- Source/FCPTools/FalloutCore/Shuttles/RoyalTitlePermitWorker_DropResourcesViaShuttle.cs
+++ Source/FCPTools/FalloutCore/Shuttles/RoyalTitlePermitWorker_DropResourcesViaShuttle.cs
@@ -1,49 +1,42 @@
 using HarmonyLib;
-using System.Collections.Generic;
-using System.Linq;
-using RimWorld;
-using Verse;
 
-namespace FCP.Core.Shuttles
+namespace FCP.Core.Shuttles;
+
+[HarmonyPatch(typeof(RoyalTitlePermitWorker_DropResources), "CallResources")]
+public static class RoyalTitlePermitWorker_DropResources_Patch
 {
-	[HarmonyPatch(typeof(RoyalTitlePermitWorker_DropResources), "CallResources")]
-	public static class RoyalTitlePermitWorker_DropResources_Patch
+	public static bool Prefix(RoyalTitlePermitWorker_DropResources __instance, IntVec3 cell, 
+		Faction ___faction, Map ___map, Pawn ___caller, bool ___free)
 	{
-		public static bool Prefix(RoyalTitlePermitWorker_DropResources __instance, IntVec3 cell)
-		{
-			// Use Traverse to access protected fields
-			var faction = Traverse.Create(__instance).Property<Faction>("faction").Value;
-			var map = Traverse.Create(__instance).Property<Map>("map").Value;
-			var caller = Traverse.Create(__instance).Property<Pawn>("caller").Value;
-			var free = Traverse.Create(__instance).Property<bool>("free").Value;
+		var extension = ___faction.def.GetModExtension<FactionModExtension>();
 			
-			var extension = faction.def.GetModExtension<FactionModExtension>();
-			if (extension != null && extension.transportShipDef != null)
-			{
-				List<Thing> list = new List<Thing>();
-				for (int i = 0; i < __instance.def.royalAid.itemsToDrop.Count; i++)
-				{
-					Thing thing = ThingMaker.MakeThing(__instance.def.royalAid.itemsToDrop[i].thingDef);
-					thing.stackCount = __instance.def.royalAid.itemsToDrop[i].count;
-					list.Add(thing);
-				}
-				if (list.Any())
-				{
-					Thing thing = ThingMaker.MakeThing(extension.transportShipDef.shipThing);
-					thing.SetFaction(faction);
-					TransportShip transportShip = TransportShipMaker.MakeTransportShip(extension.transportShipDef, list, thing);
-					transportShip.ArriveAt(cell, map.Parent);
-					transportShip.AddJobs(ShipJobDefOf.Unload, ShipJobDefOf.FlyAway);
-					Messages.Message("MessagePermitTransportDrop".Translate(faction.Named("FACTION")), new LookTargets(cell, map), MessageTypeDefOf.NeutralEvent);
-					caller.royalty.GetPermit(__instance.def, faction).Notify_Used();
-					if (!free)
-					{
-						caller.royalty.TryRemoveFavor(faction, __instance.def.royalAid.favorCost);
-					}
-				}
-				return false;
-			}
+		if (extension?.transportShipDef == null) 
 			return true;
+			
+		var contents = new List<Thing>();
+		foreach (ThingDefCountClass defCount in __instance.def.royalAid.itemsToDrop)
+		{
+			var thing = ThingMaker.MakeThing(defCount.thingDef);
+			thing.stackCount = defCount.count;
+			contents.Add(thing);
 		}
+
+		if (!contents.Any()) 
+			return false;
+			
+		Thing shipThing = ThingMaker.MakeThing(extension.transportShipDef.shipThing);
+		shipThing.SetFaction(___faction);
+			
+		TransportShip transportShip = TransportShipMaker.MakeTransportShip(extension.transportShipDef, contents, shipThing);
+		transportShip.ArriveAt(cell, ___map.Parent);
+		transportShip.AddJobs(ShipJobDefOf.Unload, ShipJobDefOf.FlyAway);
+			
+		Messages.Message("MessagePermitTransportDrop".Translate(___faction.Named("FACTION")), new LookTargets(cell, ___map), MessageTypeDefOf.NeutralEvent);
+		___caller.royalty.GetPermit(__instance.def, ___faction).Notify_Used();
+		if (!___free)
+		{
+			___caller.royalty.TryRemoveFavor(___faction, __instance.def.royalAid.favorCost);
+		}
+		return false;
 	}
 }
