aff27430ecbe3941e8763e79c445236f87c824e7
Canon <canonoverseer@gmail.com>
2026-02-26 03:13:31 +0800

    Add Precept Relation Logging Debug Action

--- Source/FCPTools/FalloutCore/Factions/Debugging/DebugActionsFCPFactions.cs
+++ Source/FCPTools/FalloutCore/Factions/Debugging/DebugActionsFCPFactions.cs
@@ -8,4 +8,29 @@ namespace FCP.Core.Factions;
 public class DebugActionsFCPFactions
 {
     public const string CategoryName = "FCP: Factions";
+    
+    [DebugAction(CategoryName, "Log Precept Relations", actionType = DebugActionType.Action)]
+    private static void LogAlignmentPrecepts()
+    {
+        var defsWithExtension = DefDatabase<PreceptDef>.AllDefsListForReading
+            .Where(def => def.HasModExtension<PreceptExtension_AlignmentRelation>())
+            .Select(def => (def: def, ext: def.GetModExtension<PreceptExtension_AlignmentRelation>()) )
+            .ToList();
+        
+        FCPLog.Message("Logging Precept Relations...");
+        
+        var sb = new StringBuilder();
+        foreach ((PreceptDef def, PreceptExtension_AlignmentRelation ext) tuple in defsWithExtension)
+        {
+            sb.AppendLine($"Def: {tuple.def.defName}");
+
+            foreach (PreceptAlignmentRelation relation in tuple.ext.alignments)
+            {
+                sb.AppendLine($" - {relation.precept.defName} -> {relation.alignment}");
+            }
+            
+            Log.Message(sb.ToString());
+            sb.Clear();
+        }
+    }
 }
