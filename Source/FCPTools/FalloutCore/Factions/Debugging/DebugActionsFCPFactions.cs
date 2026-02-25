0b0409fc06048b00c1359eeb8b1badc490d58df6
Canon <canonoverseer@gmail.com>
2026-02-26 03:12:18 +0800

    Add DebugActionsFCPFactions Class

---
+++ Source/FCPTools/FalloutCore/Factions/Debugging/DebugActionsFCPFactions.cs
@@ -0,0 +1,11 @@
+using System.Text;
+using FCP.Factions;
+using LudeonTK;
+
+namespace FCP.Core.Factions;
+
+[UsedImplicitly(ImplicitUseTargetFlags.Members)]
+public class DebugActionsFCPFactions
+{
+    public const string CategoryName = "FCP: Factions";
+}
