48c9b38a3a5bcc801efbf1f177d11e7818b88f69
Canon <canonoverseer@gmail.com>
2026-03-16 18:56:18 +0800

    Fix missing null checks and clean up CompPowerArmor

--- Source/FCPTools/FalloutCore/PowerArmor/Comps/CompPowerArmor.cs
+++ Source/FCPTools/FalloutCore/PowerArmor/Comps/CompPowerArmor.cs
@@ -2,6 +2,7 @@ using FCP.Core.Access;
 
 namespace FCP.Core.PowerArmor;
 
+[UsedImplicitly(ImplicitUseKindFlags.Assign, ImplicitUseTargetFlags.WithMembers)]
 public class CompProperties_PowerArmor : CompProperties
 {
     public HediffDef hediffOnEmptyFuel;
@@ -19,8 +20,7 @@ public class CompPowerArmor : ThingComp
 {
     public CompProperties_PowerArmor Props => base.props as CompProperties_PowerArmor;
 
-    private CompRefuelable _compRefuelable;
-    public CompRefuelable CompRefuelable => _compRefuelable ??= parent.GetComp<CompRefuelable>();
+    public CompRefuelable CompRefuelable => field ??= parent.GetComp<CompRefuelable>();
 
     public override void CompTick()
     {
@@ -28,11 +28,11 @@ public class CompPowerArmor : ThingComp
         if (parent is not Apparel apparel || apparel.Wearer is null) 
             return;
         
-        if (CompRefuelable is not CompRefuelable comp) 
+        if (CompRefuelable == null) 
             return;
                 
-        comp.ConsumeFuel(comp.P_ConsumptionRatePerTick);
-        if (Props.hediffOnEmptyFuel == null || comp.HasFuel) 
+        CompRefuelable.ConsumeFuel(CompRefuelable.P_ConsumptionRatePerTick);
+        if (Props.hediffOnEmptyFuel == null || CompRefuelable.HasFuel) 
             return;
         
         Hediff hediff = apparel.Wearer.health.hediffSet.GetFirstHediffOfDef(Props.hediffOnEmptyFuel);
@@ -49,24 +49,24 @@ public class CompPowerArmor : ThingComp
             return;
         
         Hediff hediff = pawn.health.hediffSet.GetFirstHediffOfDef(Props.hediffOnEmptyFuel);
-        if (hediff != null)
-        {
-            pawn.health.RemoveHediff(hediff);
-        }
+        if (hediff != null) pawn.health.RemoveHediff(hediff);
     }
 
     public override IEnumerable<Gizmo> CompGetWornGizmosExtra()
     {
-        if (parent is not Apparel apparel) 
+        if (parent is not Apparel apparel)
+            yield break;
+
+        if (CompRefuelable == null) 
             yield break;
         
-        var comp = parent.GetComp<CompRefuelable>();
-        if (comp.Props.showFuelGizmo && Find.Selector.SingleSelectedThing == apparel.Wearer)
+        if (CompRefuelable.Props.showFuelGizmo && Find.Selector.SingleSelectedThing == apparel.Wearer)
         {
-            var gizmoRefuelableFuelStatus = new Gizmo_SetFuelLevel(comp);
+            var gizmoRefuelableFuelStatus = new Gizmo_SetFuelLevel(CompRefuelable);
             yield return gizmoRefuelableFuelStatus;
         }
-        foreach (Gizmo gizmo in comp.CompGetGizmosExtra())
+
+        foreach (Gizmo gizmo in CompRefuelable.CompGetGizmosExtra())
         {
             yield return gizmo;
         }
