33d0d05d923ee052c4098ac32a81b4e7f6146921
Canon <canonoverseer@gmail.com>
2026-03-16 19:37:20 +0800

    AnimalUtilities Fix Possible 'Memory Leak'
    TryGetComp is a lot faster in 1.6, and the cache doesn't provide much
    advantage anymore, especially without any way to evict dead or destroyed
    pawns

--- Source/FCPTools/FalloutCore/Utilities/AnimalUtilities.cs
+++ Source/FCPTools/FalloutCore/Utilities/AnimalUtilities.cs
@@ -2,21 +2,9 @@
 
 public static class AnimalUtilities
 {
-    private static Dictionary<Pawn, CompFlyingPawn> cachedComps = new ();
-        
     public static bool IsFlyingPawn(this Pawn pawn, out CompFlyingPawn comp)
     {
-        comp = null;
-        
-        if (pawn == null) return false;
-        cachedComps ??= [];
-
-        if (!cachedComps.TryGetValue(pawn, out comp))
-        {
-            cachedComps[pawn] = pawn.TryGetComp<CompFlyingPawn>();
-        }
-
-        comp = cachedComps[pawn];
+        comp = pawn?.TryGetComp<CompFlyingPawn>();
         return comp != null;
     }
 }
