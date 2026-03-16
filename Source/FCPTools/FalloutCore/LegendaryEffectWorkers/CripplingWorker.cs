b5a64d352e39081e10d7dd6f1ee5f786c38a82e1
Canon <canonoverseer@gmail.com>
2026-03-16 19:22:57 +0800

    Change Legendary CripplingWorker/AssassinWorker to use fieldref and remove dupe checks

--- Source/FCPTools/FalloutCore/LegendaryEffectWorkers/CripplingWorker.cs
+++ Source/FCPTools/FalloutCore/LegendaryEffectWorkers/CripplingWorker.cs
@@ -1,26 +1,25 @@
-﻿using System.Reflection;
+﻿using HarmonyLib;
 
 namespace FCP.Core.LegendaryEffectWorkers;
 
 public class CripplingWorker : LegendaryEffectWorker
 {
-    public static Lazy<FieldInfo> DamageInfo_AmountInt = new(() => typeof(DamageInfo).GetField("amountInt", BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic));
+    public static readonly AccessTools.FieldRef<DamageInfo, float> DamageInfo_amountInt =
+        AccessTools.FieldRefAccess<DamageInfo, float>("amountInt");
 
     public override void Notify_ApplyToPawn(ref DamageInfo damageInfo, Pawn pawn)
     {
-        if (
-            damageInfo.HitPart.def.defName.ToLower().Contains("arm")
-            || damageInfo.HitPart.def.defName.ToLower().Contains("leg")
-            || damageInfo.HitPart.def.defName.ToLower().Contains("tentacle")
-            || damageInfo.HitPart.def.defName.ToLower().Contains("leg")
-            || damageInfo.HitPart.def.defName.ToLower().Contains("leg")
-        )
-        {
-            if (DamageInfo_AmountInt.Value != null)
-            {
-                float damageAmount = (float)DamageInfo_AmountInt.Value.GetValue(damageInfo);
-                DamageInfo_AmountInt.Value.SetValueDirect(__makeref(damageInfo), damageAmount * 1.5f);
-            }
-        }
+        if (!ContainsLimb(damageInfo)) 
+            return;
+
+        float damageAmount = DamageInfo_amountInt(damageInfo); //(float)DamageInfo_AmountInt.Value.GetValue(damageInfo);
+        DamageInfo_amountInt(damageInfo) = damageAmount * 1.5f;
+    }
+
+    private static bool ContainsLimb(DamageInfo damageInfo)
+    {
+        return damageInfo.HitPart.def.defName.ToLower().Contains("arm")
+               || damageInfo.HitPart.def.defName.ToLower().Contains("tentacle")
+               || damageInfo.HitPart.def.defName.ToLower().Contains("leg");
     }
 }
