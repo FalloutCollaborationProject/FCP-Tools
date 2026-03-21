ae99720937a85774baf8e9eec2697883e13dadb3
Canon <canonoverseer@gmail.com>
2026-03-21 13:13:53 +0800

    Clean up CompApparelRequirement keys

--- Source/FCPTools/FalloutCore/PowerArmor/Comps/CompApparelRequirement.cs
+++ Source/FCPTools/FalloutCore/PowerArmor/Comps/CompApparelRequirement.cs
@@ -1,21 +1,32 @@
 namespace FCP.Core.PowerArmor;
 
 // TODO look at moving this somewhere else
+[UsedImplicitly(ImplicitUseKindFlags.Assign, ImplicitUseTargetFlags.WithMembers)]
 public class CompProperties_ApparelRequirement : CompProperties
 {
     public List<ThingDef> requiredApparels;
     public TraitDef requiredTrait;
 
-    public CompProperties_ApparelRequirement() => compClass = typeof(CompApparelRequirement);
+    public CompProperties_ApparelRequirement()
+    {
+        this.compClass = typeof(CompApparelRequirement);
+    }
 }
 
 public class CompApparelRequirement : ThingComp
 {
+    private static class Keys
+    {
+        public const string RequiresTrait = "FCP_ApparelRequirement.RequiresTrait";
+        public const string RequiresApparel = "FCP_ApparelRequirement.RequiresApparel";
+        public const string AnyOfApparel = "FCP_ApparelRequirement.RequiresAnyOfApparel";
+    }
+    
     public CompProperties_ApparelRequirement Props => base.props as CompProperties_ApparelRequirement;
 
     public bool HasRequiredApparel(Pawn pawn)
     {
-        return Props.requiredApparels is null || pawn.apparel.WornApparel.Any(y => Props.requiredApparels.Contains(y.def));
+        return Props.requiredApparels is null || pawn.apparel.WornApparel.Any(apparel => Props.requiredApparels.Contains(apparel.def));
     }
 
     public bool HasRequiredTrait(Pawn pawn)
@@ -43,16 +54,13 @@ public class CompApparelRequirement : ThingComp
     {
         if (!HasRequiredTrait(pawn))
         {
-            return "RR.RequiresTrait".Translate(Props.requiredTrait.degreeDatas[0].label);
+            return Keys.RequiresTrait.Translate(Props.requiredTrait.degreeDatas[0].label);
         }
         if (!HasRequiredApparel(pawn))
         {
-            if (Props.requiredApparels.Count == 1)
-            {
-                return "RR.RequiresApparel".Translate(Props.requiredApparels[0].label);
-            }
-
-            return "RR.RequiresApparelsAnyOf".Translate(string.Join(", ", Props.requiredApparels.Select(x => x.label)));
+            return Props.requiredApparels.Count == 1 
+                ? Keys.RequiresApparel.Translate(Props.requiredApparels[0].label) 
+                : Keys.AnyOfApparel.Translate(Props.requiredApparels.Select(def => def.label).ToCommaListOr());
         }
 
         return true;
